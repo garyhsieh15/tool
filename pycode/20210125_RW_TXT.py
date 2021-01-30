87ef2312a1b848e9595627e574a89d6a3f711c89
garyhsieh <garyhsieh@garyhsiehs-iMac.AirDream>
2021-01-31 02:38:44 +0800

    add comment for r/w

--- pycode/20210125_RW_TXT.py
+++ pycode/20210125_RW_TXT.py
@@ -4,31 +4,50 @@ FILE_NAME = "./file/TAP024.txt"
 
 FILE_NAME_W = "./file/TAP024_W.txt"
 
+"""
+open(路徑+檔名,讀寫模式) 
+r:讀, w:新建(會覆蓋原有檔案),a追加,b二進位制檔案.
+
+讀寫模式的型別有：
+rU 或 Ua 以讀方式開啟, 同時提供通用換行符支援 (PEP 278)
+w      以寫方式開啟，
+a      以追加模式開啟 (從 EOF 開始, 必要時建立新檔案)
+
+r      以讀模式開啟
+w      以寫模式開啟 (參見 w)
+a      以寫模式開啟 (參見 a)
+
+rb     以二進位制讀模式開啟
+wb     以二進位制寫模式開啟 (參見 w)
+ab     以二進位制追加模式開啟 (參見 a)
+"""
+
 # -------------------------------------------------------------------------------------
 # name       : read_acc_history()
 # description: read history of acc data.
 #
+
 # date       : 20210126
 # author     : garyhsieh
 # -------------------------------------------------------------------------------------
-def read_acc_history():
+def read_acc_history(_fname):
 
     try:
         ACC_HIS = []
-        with open(FILE_NAME, 'r') as f:
+        with open(_fname, 'r') as f:
             ACC_HIS = f.readlines()
 
     except FileNotFoundError:
-        print(">>> couldn't find", FILE_NAME + '.')
+        print(">>> couldn't find", "\"" + _fname + "\".")
 
     except IsADirectoryError:
-        print(">>> ", FILE_NAME, "is a directory")
+        print(">>> ", "\"" + _fname + "\"", "is a directory")
 
     except:
-        print(">>> error !!, couldn't read", FILE_NAME + '.')
+        print(">>> error !!, couldn't read", "\"" + _fname + "\"" + '.')
 
     else:
-        print(">>> find", FILE_NAME + '.')
+        print(">>> find", "\"" + _fname + '".')
 
     finally:
         print("do no matter what")
@@ -74,16 +93,16 @@ def read_acc_history():
 # date       : 20210126
 # author     : garyhsieh
 # -------------------------------------------------------------------------------------
-def write_acc_history():
+def write_acc_history(_fname):
     
     text = "89.995     4.299     5.574     0.049\n"
 
-    with open(FILE_NAME_W, 'a') as fw:
+    with open(_fname, 'a') as fw:
         fw.write(text)
 
 if __name__ == "__main__":
-    t, up, ns, ew = read_acc_history()
+    t, up, ns, ew = read_acc_history(FILE_NAME)
     print("show time: ", t)
 
-    #write_acc_history()
+    #write_acc_history(FILE_NAME_W)
 
